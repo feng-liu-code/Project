b8f697f68624f10bc322c7112d9d498ba45db956
feng.liu <feng.liu@goodwe.com>
2026-09-03 19:30:55 +0800

    DigitSum

---
+++ C++/01-OverviewOfC++/DigitSum/DigitSum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int digitSum(int n);
+
+int main()
+{
+	cout << "Program to test the digitSum function " << endl;
+	while(true)
+	{
+		int n;
+		cout << "Enter a number (use -1 to stop): ";
+		cin >> n;
+		if(n < 0)
+		{
+			break;
+		}
+		cout << "digitSum(" << n << ") = " << digitSum(n) << endl;
+	}
+	return  0;
+}
+
+/*
+Return the sum of the digits in the number n
+*/
+int digitSum(int n)
+{
+	int sum = 0;
+	while(n > 0)
+	{
+		sum += n % 10;
+		n/=10;
+	}
+	return sum;
+}
